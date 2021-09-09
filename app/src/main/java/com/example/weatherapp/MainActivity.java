dac5cbfb4ccf7b162f0d5ebb5dcb6da6138e59b1
koloversh <ImmortalNickolas@yandex.ru>
2021-09-09 16:14:06 +0300

    Internet permission

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weatherapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText user_location;
     private Button search_button;
     private TextView search_info;
+    String key = "998e60ead5535b18eded3c1ae586b811";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +32,26 @@ public class MainActivity extends AppCompatActivity {
                     //Если не ввёл, то всплывает окно с запросом на ввод названия.
                     Toast.makeText(MainActivity.this, R.string.Arrr, Toast.LENGTH_LONG).show();
                 } else {
+                    String city = user_location.getText().toString();
+                    String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city
+                            + "&APPID=" + key + "&units=metric&lang=ru";
+
+                    new WeatherData().execute(url);
 
                 }
             }
         });
+    }
+
+    private class WeatherData extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
 }
